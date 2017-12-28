8a89f08bc018b47f6beef64bcd18a622be2943bd
retishp <retishp@users.noreply.github.com>
2017-12-28 15:14:31 -0500

    Delete NR.ps1

--- NR.ps1
+++
@@ -1,7 +0,0 @@
-Login-AzureRmAccount
-
-$sub1 = Get-AzureRmSubscription -SubscriptionId "96e98080-e35c-4963-a6e8-9748d470c233" # 'TSS Team Sandbox' # | Select-AzureRmSubscription
-
-Select-AzureRmSubscription -SubscriptionObject $sub1[0].SubscriptionId
-
-New-AzureRmResourceGroupDeployment -Name NRDeployment -ResourceGroupName rperum -TemplateFile template.json

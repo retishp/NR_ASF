7204b381d1705deb5d4ea021039c580df556c16d
retishp <retishp@users.noreply.github.com>
2017-12-28 18:19:49 -0500

    Add files via upload

---
+++ automateNR.ps1
@@ -0,0 +1,10 @@
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+Unzip ".\NewRelic.Agent.Installer.zip" "C:\NRAgent"
+& "c:\NRAgent\install.cmd" -LicenseKey 5b770a55f69576aeebc63fb31ee3fb73d174ab44 -InstrumentAll
